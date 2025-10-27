2cb7d5f8dc2ca4dbd08dd6ee61a0a5b567ffec18
iakashdev <iakashperingethdev@gmail.com>
2025-10-27 16:12:18 +0530

    path changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* <Route path="/" element={<App />} /> */}
+
+         <Route path="/byakashperingeth" element={<App />} />
         <Route path="/myjourney" element={<Myjourney />} />
       </Routes>
     </Router>
