06ab7b848edde7edecbe0ad48b639c2741aa411e
akash-olivo <akash.p@olivotech.com>
2025-11-18 11:39:12 +0530

    stars implemeted commit

--- iakashperingeth/src/App.jsx
+++ iakashperingeth/src/App.jsx
@@ -1,23 +1,46 @@
-import { useState } from 'react'
- //import mylogo from './assets/myimage.png'
- import mylogo from './assets/3.png'
-import 'bootstrap/dist/css/bootstrap.min.css' // Import Bootstrap
-
+import { useState, useEffect } from 'react'
+import mylogo from './assets/3.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import './App.css'
 
 function App() {
+  const [stars, setStars] = useState([])
+
+  useEffect(() => {
+    const generatedStars = Array.from({ length: 80 }, (_, i) => ({
+      id: i,
+      top: `${Math.random() * 100}%`,
+      left: `${Math.random() * 100}%`,
+      size: `${Math.random() * 2 + 1}px`,
+    }))
+    setStars(generatedStars)
+  }, [])
+
   return (
     <div className='base-wrapper'>
+      {/* Stars Background */}
+      <div className='stars-background'>
+        {stars.map(star => (
+          <div
+            key={star.id}
+            className='star'
+            style={{
+              top: star.top,
+              left: star.left,
+              width: star.size,
+              height: star.size,
+            }}
+          />
+        ))}
+      </div>
+      
      <div className='top-container'>
       <a className='optns'>Home</a>
       <a className='optns'>Connect +</a>
       <a className='optns'>Projects</a>
       <a className='optns'>Path</a>
-
-
       </div>
-
 
       <div className='bottom-container bg-white '>
         <div className='about '>
@@ -25,22 +48,14 @@ function App() {
           Hi,im Akash Peringeth
           </p>
           <h1 className='line2 text-white'>DEVELOPER</h1>
-
           <p className="line3 fs-1  ">
           HERE MY CODE BECOMES A QUIET FILIM ON PAPER FOR EVERY DREAM
           </p>
-
-
         </div>
          <div className='aboutimg '>
           <img src={mylogo} alt="My Image"  className='Pimage'/>
-
-
         </div>
       </div>
-
-
-      
     </div>
   )
 }
